6e89e34922d2a363bab69d542c7534c6c8071ef0
Efrencito bb <19302069@utfv.edu>
2022-11-10 19:02:06 -0600

    framework base listo para comenzar (firebase, react-bootstrap, archivo de credenciales)

---
+++ src/credenciales.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGu-UaApF4qxFLxFRsof-2y57hCulexPk",
+  authDomain: "pasteleria-sistema.firebaseapp.com",
+  projectId: "pasteleria-sistema",
+  storageBucket: "pasteleria-sistema.appspot.com",
+  messagingSenderId: "505155872343",
+  appId: "1:505155872343:web:996123696a341bb0d86512"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
