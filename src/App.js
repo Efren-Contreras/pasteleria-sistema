33c34fb101a78f48dde6ad1750aff4498acaafb8
Efrencito bb <19302069@utfv.edu>
2022-11-15 14:12:30 -0600

    pwa para pruebas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 /**Vistas principales */
 import Login from "./componentes/pantallas/login";
@@ -67,12 +67,45 @@ function App() {
     }
   });
 
+  const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
 
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
+
+  useEffect(() => {
+  window.addEventListener("beforeinstallprompt", (event) => {
+    // Prevent the mini-infobar from appearing on mobile.
+    event.preventDefault();
+    console.log("👍", "beforeinstallprompt", event);
+    // Stash the event so it can be triggered later.
+    window.deferredPrompt = event;
+    // Remove the 'hidden' class from the install button container.
+    setIsReadyForInstall(true);
+  });
+}, []);
 
   return (
     /**dependiendo si hay sesion iniciada o no redirige al inicio o login */
     
     <>
+    {isReadyForInstall && <button onClick={downloadApp}>DESCARGAR</button>}
       {usuarioGlobal ? (
         <Home user={usuarioGlobal} />
       ) : (
--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 //ANDY
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 /**resolver problema del router */
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
@@ -26,3 +28,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    // Corremos este código si hay una nueva versión de nuestra app
+    // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
